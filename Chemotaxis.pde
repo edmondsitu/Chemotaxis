bdda715cc5f923ba2bb0baf26df07bbac39c2a64
unknown <lisitu4@697-334-Bywater.sf.k12.internal>
2015-09-17 10:26:21 -0700

    color for each

--- Chemotaxis.pde
+++ Chemotaxis.pde
@@ -1,28 +1,28 @@
  //declare bacteria variables here 
- int myX;
- int myY;
- int bactColor1;
- int bactColor2;
- int bactColor3;
- Bacteria bob; 
+
+ Bacteria [] colony;
 
  void setup()   
  {     
  	//initialize bacteria variables here
  	size(500,500);
- 	bob = new Bacteria(myX,myY);
- 	myX = 250;
- 	myY = 250;  
+ 	colony = new Bacteria [100];
+
+
+ 	for(int i = 0; i < colony.length; i++)
+ 	{
+ 		colony [i] = new Bacteria(250,250); 
+ 	}
  }   
  void draw()   
  {    
  	//move and show the bacteria
  	background(0);
- 	bob.show();
- 	bob.move();
- 	bactColor1 = (int)(Math.random()*255)+1;
- 	bactColor2 = (int)(Math.random()*255)+1;
- 	bactColor3 = (int)(Math.random()*255)+1;
+ 	for(int i = 0; i < colony.length; i++)
+ 	{
+ 		colony[i].move();
+ 		colony[i].show();
+ 	}
  }
 
  class Bacteria    
@@ -30,17 +30,22 @@
  	//lots of java!
 
  	//contructor code
+ 	int myX;
+ 	int myY;
+ 	int myColor;
  	Bacteria(int x, int y)
  	{
  		myX = x;
  		myY = y;
+ 		myColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
  	}
+
  	//show function for the bacteria
  	void show()
  	{
  		noStroke();
- 		fill(bactColor1,bactColor2,bactColor3);
- 		ellipse(myX,myY,20,20);
+ 		fill(myColor);
+ 		ellipse(myX,myY,10,10);
  	}
  	//random walk for bacteria
  	void move()
--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -15,30 +15,30 @@ import java.io.IOException;
 public class Chemotaxis extends PApplet {
 
  //declare bacteria variables here 
- int myX;
- int myY;
- int bactColor1;
- int bactColor2;
- int bactColor3;
- Bacteria bob; 
+
+ Bacteria [] colony;
 
  public void setup()   
  {     
  	//initialize bacteria variables here
  	size(500,500);
- 	bob = new Bacteria(myX,myY);
- 	myX = 250;
- 	myY = 250;  
+ 	colony = new Bacteria [100];
+
+
+ 	for(int i = 0; i < colony.length; i++)
+ 	{
+ 		colony [i] = new Bacteria(250,250); 
+ 	}
  }   
  public void draw()   
  {    
  	//move and show the bacteria
  	background(0);
- 	bob.show();
- 	bob.move();
- 	bactColor1 = (int)(Math.random()*255)+1;
- 	bactColor2 = (int)(Math.random()*255)+1;
- 	bactColor3 = (int)(Math.random()*255)+1;
+ 	for(int i = 0; i < colony.length; i++)
+ 	{
+ 		colony[i].move();
+ 		colony[i].show();
+ 	}
  }
 
  class Bacteria    
@@ -46,17 +46,22 @@ public class Chemotaxis extends PApplet {
  	//lots of java!
 
  	//contructor code
+ 	int myX;
+ 	int myY;
+ 	int myColor;
  	Bacteria(int x, int y)
  	{
  		myX = x;
  		myY = y;
+ 		myColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
  	}
+
  	//show function for the bacteria
  	public void show()
  	{
  		noStroke();
- 		fill(bactColor1,bactColor2,bactColor3);
- 		ellipse(myX,myY,20,20);
+ 		fill(myColor);
+ 		ellipse(myX,myY,10,10);
  	}
  	//random walk for bacteria
  	public void move()
