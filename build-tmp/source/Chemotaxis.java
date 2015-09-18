e529d88e018799880d4c5383d7d5319260d7a8c3
unknown <lisitu4@697-334-Bywater.sf.k12.internal>
2015-09-18 10:45:48 -0700

    Follow the mouse

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -21,23 +21,25 @@ public class Chemotaxis extends PApplet {
  public void setup()   
  {     
  	//initialize bacteria variables here
- 	size(500,500);
- 	colony = new Bacteria [100];
+ 	size(600,600);
+ 	colony = new Bacteria [500];
 
 
  	for(int i = 0; i < colony.length; i++)
  	{
- 		colony [i] = new Bacteria(250,250); 
+ 		colony [i] = new Bacteria((int)(Math.random()*590),(int)(Math.random()*590)); 
  	}
  }   
  public void draw()   
  {    
  	//move and show the bacteria
- 	background(0);
+ 	background(0,0,0);
+ 	ellipse(mouseX, mouseY, 50, 50);
  	for(int i = 0; i < colony.length; i++)
  	{
  		colony[i].move();
  		colony[i].show();
+ 		colony[i].change();
  	}
  }
 
@@ -49,11 +51,13 @@ public class Chemotaxis extends PApplet {
  	int myX;
  	int myY;
  	int myColor;
+ 	int mySize;
  	Bacteria(int x, int y)
  	{
  		myX = x;
  		myY = y;
  		myColor = color((int)(Math.random()*256),(int)(Math.random()*256),(int)(Math.random()*256));
+ 		mySize = (int)(Math.random()*6)+5;
  	}
 
  	//show function for the bacteria
@@ -61,13 +65,36 @@ public class Chemotaxis extends PApplet {
  	{
  		noStroke();
  		fill(myColor);
- 		ellipse(myX,myY,10,10);
+ 		ellipse(myX,myY,mySize,mySize);
  	}
  	//random walk for bacteria
  	public void move()
  	{
  		myX = myX + (int)(Math.random()*3)-1;
  		myY = myY + (int)(Math.random()*3)-1;
+ 		if( mouseX > myX)
+ 		{
+ 			myX = myX + (int)(Math.random()*5)-1;
+ 		}
+ 		if( mouseX < myX)
+ 		{
+ 			myX = myX + (int)(Math.random()*3)-2;
+ 		}
+ 		if( mouseY > myY)
+ 		{
+ 			myY = myY + (int)(Math.random()*5)-1;
+ 		}
+ 		if( mouseY < myY)
+ 		{
+ 			myY = myY + (int)(Math.random()*3)-2;
+ 		}
+ 	}
+ 	public void change()
+ 	{
+ 		if(get(myX,myY) != color(myColor))
+ 		{
+ 			mySize = mySize + 1;
+ 		}
  	}
 
  }    
